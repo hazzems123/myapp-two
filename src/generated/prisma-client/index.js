d3a01e7a37af21e5c516728cceba23b7bcb790ea
root <root@DESKTOP-SBGP4QS.localdomain>
2019-06-26 08:03:07 +0200

    myapp-two

---
+++ src/generated/prisma-client/index.js
@@ -0,0 +1,21 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var prisma_lib_1 = require("prisma-client-lib");
+var typeDefs = require("./prisma-schema").typeDefs;
+
+var models = [
+  {
+    name: "Post",
+    embedded: false
+  },
+  {
+    name: "Product",
+    embedded: false
+  }
+];
+exports.Prisma = prisma_lib_1.makePrismaClientClass({
+  typeDefs,
+  models,
+  endpoint: `https://eu1.prisma.sh/public-crackmole-732/my-apptwo/dev`
+});
+exports.prisma = new exports.Prisma();
---
+++ src/index.js
@@ -0,0 +1,52 @@
+const { GraphQLServer } = require('graphql-yoga')
+const { prisma } = require('./generated/prisma-client')
+
+const resolvers = {
+  Query: {
+    feed: (parent, args, context) => {
+      return context.prisma.posts({ where: { published: true } })
+    },
+    drafts: (parent, args, context) => {
+      return context.prisma.posts({ where: { published: false } })
+    },
+    post: (parent, { id }, context) => {
+      return context.prisma.post({ id })
+    },
+    product:(parent, args, context) => {
+      return context.prisma.products()
+  },
+  Mutation: {
+    createDraft(parent, { title, content }, context) {
+      return context.prisma.createPost({
+        title,
+        content,
+      })
+    },
+    deletePost(parent, { id }, context) {
+      return context.prisma.deletePost({ id })
+    },
+    publish(parent, { id }, context) {
+      return context.prisma.updatePost({
+        where: { id },
+        data: { published: true },
+      })
+    },
+    createProduct (parent, {name, price, sale, likes}, context) {
+     return context.prisma.createProduct({
+       name,
+       price,
+       sale,
+       likes,
+     })
+  },
+}
+
+const server = new GraphQLServer({
+  typeDefs: './src/schema.graphql',
+  resolvers,
+  context: {
+    prisma,
+  },
+})
+
+server.start(() => console.log('Server is running on http://localhost:5000'))
